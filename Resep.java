92a7f8309c3992fd07a145d9915888dc1d663e44
xxsitiafrida <sitiafrida1234@gmail.com>
2022-11-03 07:59:00 +0700

    v1

---
+++ Resep.java
@@ -0,0 +1,7 @@
+public class Resep {
+    public static void main(String[] args) {
+        System.out.println("Resep Patty");
+        System.out.println("1. Bawang");
+        System.out.println("2. Timun");
+    }
+}
